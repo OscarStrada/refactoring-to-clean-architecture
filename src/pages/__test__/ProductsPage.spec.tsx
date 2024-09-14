d1c959adbf450c3f27c95eeb970bd4829bca00ca
Oscar Estrada <oestrada@ia.com.mx>
2024-09-13 20:25:05 -0600

    test(unit): write unit test to check App is render

--- src/pages/__test__/ProductsPage.spec.tsx
+++ src/pages/__test__/ProductsPage.spec.tsx
@@ -1,3 +1,9 @@
 import { test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "../../App";
+
+test("Loads and displays title", async () => {
+    render(<App />);
+
+    await screen.findAllByRole("heading", { name: "Product price updater" });
+});
