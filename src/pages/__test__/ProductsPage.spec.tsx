7aff8b61a8c97c296456410d409751dbb030893d
Oscar Estrada <arturostrada2020@gmail.com>
2024-09-15 14:52:11 -0600

    test(unit): add mockWebServer to intercept api response

--- src/pages/__test__/ProductsPage.spec.tsx
+++ src/pages/__test__/ProductsPage.spec.tsx
@@ -1,14 +1,37 @@
-import { test } from "vitest";
+import { afterAll, beforeAll, describe, test } from "vitest";
 import { render, RenderResult, screen } from "@testing-library/react";
 import { AppProvider } from "../../context/AppProvider";
 import { ProductsPage } from "../ProductsPage";
+import { MockWebServer } from "../../tests/mockWebServer";
+import { afterEach } from "node:test";
+import productsResponse from "./data/productsResponse.json";
 
-test("Loads and displays title", async () => {
-    renderComponent(<ProductsPage />);
+const mockWebServer = new MockWebServer();
 
-    await screen.findAllByRole("heading", { name: "Product price updater" });
+describe("ProductsPage", () => {
+    beforeAll(() => mockWebServer.start());
+    afterEach(() => mockWebServer.resetHandlers());
+    afterAll(() => mockWebServer.close());
+
+    test("Loads and displays title", async () => {
+        getProducts();
+        renderComponent(<ProductsPage />);
+
+        await screen.findAllByRole("heading", { name: "Product price updater" });
+    });
 });
 
 const renderComponent = (component: React.ReactNode): RenderResult => {
     return render(<AppProvider>{component}</AppProvider>);
+};
+
+const getProducts = () => {
+    mockWebServer.addRequestHandlers([
+        {
+            method: "get",
+            endpoint: "https://fakestoreapi.com/products",
+            httpStatusCode: 200,
+            response: productsResponse,
+        },
+    ]);
 };
