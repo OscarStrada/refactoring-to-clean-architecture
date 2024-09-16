7d0fc5533c3a5c0bb1f87473d5e2c367f4c7a59f
Oscar Estrada <arturostrada2020@gmail.com>
2024-09-15 18:33:26 -0600

    feat(util): create productPage fixture

---
+++ src/pages/__test__/products-page/productsPage.fixture.ts
@@ -0,0 +1,27 @@
+import { MockWebServer } from "@/tests/mockWebServer";
+import productsResponse from "./data/productsResponse.json";
+import { RemoteProduct } from "@/api/StoreApi";
+
+export const getProducts = (mockWebServer: MockWebServer): RemoteProduct[] => {
+    mockWebServer.addRequestHandlers([
+        {
+            method: "get",
+            endpoint: "https://fakestoreapi.com/products",
+            httpStatusCode: 200,
+            response: productsResponse,
+        },
+    ]);
+
+    return productsResponse;
+};
+
+export const getEmptyProducts = (mockWebServer: MockWebServer) => {
+    mockWebServer.addRequestHandlers([
+        {
+            method: "get",
+            endpoint: "https://fakestoreapi.com/products",
+            httpStatusCode: 200,
+            response: [],
+        },
+    ]);
+};
